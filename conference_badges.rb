a27de875fc3b1062fea160b69e22637afc6581de
Sarah Indano <sarah.indano@gmail.com>
2022-04-04 15:53:10 -0400

    commit tree 3b11f8fca08dd9c69cceaa227c7c50d1fea31268

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,39 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker (name)
+    "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator (attendees)
+    batched_badges = attendees.map do |attendee|
+        "Hello, my name is #{attendee}."
+    end
+end
+
+
+# def assign_rooms (attendees)
+#     attendee_names = attendees.map do |attendee|
+#         "Hello, #{attendee}! You'll be assigned to room #{attendees.index(attendee)+1}!"
+#     end
+# end
+
+def assign_rooms (attendees)
+  rooms = []
+  attendees.each.with_index(1) do |attendee, room|
+    rooms.push "Hello, #{attendee}! You'll be assigned to room #{room}!"
+  end
+  rooms
+end
+
+def printer (attendees)
+    (batch_badge_creator(attendees)).each do |received_badge|
+        puts received_badge
+    end
+    (assign_rooms(attendees)).each do |room_assignment|
+        puts room_assignment
+    end
+end
+
+
